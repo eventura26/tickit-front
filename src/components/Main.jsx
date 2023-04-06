13392fbd838a9c590d9116e146f403b6b099e9b3
Elliott Ventura <elliott.ventura26@gmail.com>
2023-04-05 19:00:26 -0500

    fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,8 +6,6 @@ import Events from './Events'
 import EventDetail from './EventDetail'
 import Venues from './Venues'
 import VenueDetail from './VenueDetail'
-import Login from './Login'
-import Register from './Register'
 import SearchResults from './SearchResults'
 import Tickets from './Tickets'
 import Confirmation from './Confirmation'
@@ -23,8 +21,6 @@ export default function Main(){
                 <Route path = "/artists/:id" element = {<ArtistDetail/>}/>
                 <Route path= "/events" element= {<Events/>}/>
                 <Route path = "/events/:id" element = {<EventDetail/>}/>
-                <Route path = "/login" element = {<Login/>}/>
-                <Route path = "/register" element = {<Register/>}/>
                 <Route path = "/tickets/:id" element = {<Tickets/>}/>
                 <Route path = "/confirmation/" element = {<Confirmation/>}/>
                 <Route path= "/searchresults/" element= {<SearchResults/>}/>
